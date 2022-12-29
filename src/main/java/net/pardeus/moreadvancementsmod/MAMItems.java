96843cada2ec39dc50d3589238766e87b2ce122d
ParDeus <marcoasfonseca@outlook.com>
2022-12-29 19:20:24 +0000

    Upload

--- src/main/java/net/pardeus/moreadvancementsmod/MAMItems.java
+++ src/main/java/net/pardeus/moreadvancementsmod/MAMItems.java
@@ -1,9 +1,11 @@
 package net.pardeus.moreadvancementsmod;
 
+
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class MAMItems extends Items {
 
@@ -12,13 +14,15 @@ public class MAMItems extends Items {
     public static final Item DOLPHINS_GRACE = new Item(new Item.Settings());
     public static final Item CONDUIT_ICON = new Item(new Item.Settings());
     public static final Item RAVAGER_FACE = new Item(new Item.Settings());
+    public static final Item HERO_ICON = new Item(new Item.Settings());
 
     public static void register() {
 
-        Registry.register(Registry.ITEM, new Identifier("moreadvancementsmod", "guardian_face"), GUARDIAN_FACE);
-        Registry.register(Registry.ITEM, new Identifier("moreadvancementsmod", "elder_guardian_face"), ELDER_GUARDIAN_FACE);
-        Registry.register(Registry.ITEM, new Identifier("moreadvancementsmod", "dolphins_grace"), DOLPHINS_GRACE);
-        Registry.register(Registry.ITEM, new Identifier("moreadvancementsmod", "conduit_icon"), CONDUIT_ICON);
-        Registry.register(Registry.ITEM, new Identifier("moreadvancementsmod", "ravager_face"), RAVAGER_FACE);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "guardian_face"), GUARDIAN_FACE);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "elder_guardian_face"), ELDER_GUARDIAN_FACE);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "dolphins_grace"), DOLPHINS_GRACE);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "conduit_icon"), CONDUIT_ICON);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "ravager_face"), RAVAGER_FACE);
+        Registry.register(Registries.ITEM, new Identifier("moreadvancementsmod", "hero_icon"), HERO_ICON);
     }
 }
